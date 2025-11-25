488fad763f7b0642891ae33ca1bd19a6cc6a4bb2
zorgoalex <zorgoalex@gmail.com>
2025-11-25 22:27:56 +0500

    feat(calendar): Removed internal scrolls in day columns. Row height is now determined by the tallest day in the row.

--- src/components/CustomSider.tsx
+++ src/components/CustomSider.tsx
@@ -280,7 +280,8 @@ export const CustomSider: React.FC = () => {
             fontWeight: 600,
           }}
         >
-          ERP v.0.5
+          <span>ERP Zhihaz </span>
+          <span style={{ fontSize: "0.8em", fontWeight: 400 }}>v.0.5</span>
         </Title>
       </div>
 
@@ -446,4 +447,3 @@ export const CustomSider: React.FC = () => {
 };
 
 export default CustomSider;
-
